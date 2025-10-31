73dd57c2cffb7390092a4b5e31832edc3d75b267
sabriyasar <info@sabriyasar.com.tr>
2025-10-31 23:29:14 +0300

    updated

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-const connectDB = require('./utils/db')
-const productsRouter = require('./routes/products')
+const connectDB = require('./utils/db');
+const productsRouter = require('./routes/products');
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+// CORS
+const allowedOrigins = [
+  'http://localhost:3000',               // local frontend
+  'https://stock-panel-six.vercel.app', // prod frontend
+];
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5000'], // frontend ve backend origin
+  origin: function(origin, callback) {
+    // origin null olabilir (Postman, curl vs için)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-app.use(express.json())
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB bağlantısı
-connectDB()
+connectDB();
 
 // Routes
-app.use('/api/products', productsRouter)
+app.use('/api/products', productsRouter);
 
 // Server start
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
