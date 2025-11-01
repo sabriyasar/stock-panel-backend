39091c41ff0ce80c65e201ab91683c9b85d58326
sabriyasar <info@sabriyasar.com.tr>
2025-11-02 00:03:06 +0300

    updated cors 2

--- config/cors.js
+++ config/cors.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3008', // admin paneli
     'http://localhost:5000', // backend
-    'https://stock-panel-js.vercel.app', // kullanıcı paneli
+    'stock-user-panel.vercel.app', // kullanıcı paneli
     'https://stock-admin-panel.vercel.app' // admin paneli
   ];
   
