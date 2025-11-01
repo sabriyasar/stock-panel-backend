6f0eaa0728c2a81b5f86638f7ae96ac67b6d8b5c
sabriyasar <info@sabriyasar.com.tr>
2025-11-02 01:05:53 +0300

    updated server.js 4

--- server.js
+++ server.js
@@ -6,46 +6,6 @@ const path = require("path");
 
 const connectDB = require("./utils/db");
 const productsRouter = require("./routes/products");
-const usersRouter = require("./routes/admin/user");
-const authRouter = require("./routes/admin/auth");
-
-const app = express();
-const PORT = process.env.PORT || 4550;
-
-// Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// CORS
-app.use(cors(corsOptions));
-
-// Preflight OPTIONS için tüm yolları açık bırak
-app.options("*", cors(corsOptions));
-
-// Static files
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// MongoDB bağlantısı
-connectDB();
-
-// Routes
-app.use("/api/products", productsRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/auth", authRouter);
-
-// Server start
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-/* require("dotenv").config();
-const express = require("express");
-const corsOptions = require("./config/cors");
-const cors = require("cors");
-const path = require("path");
-
-const connectDB = require("./utils/db");
-const productsRouter = require("./routes/products");
 
 // ADMIN PANEL ROUTES
 const adminRouter = require("./routes/admin/user");
@@ -84,6 +44,46 @@ app.use("/api/admin/auth", adminAuthRouter);
 // USER PANEL
 app.use("/api/users/users", usersRouter);
 app.use("/api/users/auth", usersAuthRouter);
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+/* require("dotenv").config();
+const express = require("express");
+const corsOptions = require("./config/cors");
+const cors = require("cors");
+const path = require("path");
+
+const connectDB = require("./utils/db");
+const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/admin/user");
+const authRouter = require("./routes/admin/auth");
+
+const app = express();
+const PORT = process.env.PORT || 4550;
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// CORS
+app.use(cors(corsOptions));
+
+// Preflight OPTIONS için tüm yolları açık bırak
+app.options("*", cors(corsOptions));
+
+// Static files
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// MongoDB bağlantısı
+connectDB();
+
+// Routes
+app.use("/api/products", productsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 // Server start
 app.listen(PORT, () => {
