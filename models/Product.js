07c3a76918d142e30d3de4026c849cb8fcb330ba
sabriyasar <info@sabriyasar.com.tr>
2025-11-03 00:05:10 +0300

    updated 11

--- models/Product.js
+++ models/Product.js
@@ -5,14 +5,17 @@ const ProductSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
   image: {
-    data: { type: Buffer },       // opsiyonel
-    contentType: { type: String },// opsiyonel
+    data: Buffer,       // opsiyonel
+    contentType: String, // opsiyonel
   },
   barcode: {
     type: String,
-    unique: true,
-    sparse: true, // null veya undefined olursa unique index hatası çıkmaz
+    unique: true,   // benzersiz olmalı
+    sparse: true,   // null veya undefined olursa unique index hatası çıkmaz
   },
 });
+
+// Gerekirse manuel index oluşturma (Mongoose otomatik de oluşturur)
+ProductSchema.index({ barcode: 1 }, { unique: true, sparse: true });
 
 module.exports = mongoose.model('Product', ProductSchema);
